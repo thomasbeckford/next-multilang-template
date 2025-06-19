66e0e3e07a3cec7c5fda6408547e70b506c7e112
Thomas Beckford <tebeckford@gmail.com>
2025-06-19 02:18:45 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,14 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const nextConfig: NextConfig = {}
 
 const withNextIntl = createNextIntlPlugin()
+
+nextConfig.rewrites = async () => {
+  return [
+    {
+      source: '/studio/:path*',
+      destination: '/studio/index.html',
+    },
+  ]
+}
+
 export default withNextIntl(nextConfig)
