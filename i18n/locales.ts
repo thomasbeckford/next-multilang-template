5ec305518cd4a58fc52aa2d00605c5be99120b93
Thomas Beckford <tebeckford@gmail.com>
2025-06-24 22:46:23 +0100

    chore: remove locales

--- i18n/locales.ts
+++ i18n/locales.ts
@@ -30,34 +30,10 @@ const locales = [
     imageUrl: 'https://flagcdn.com/br.svg',
   },
   {
-    label: 'Italiano',
-    value: 'it',
-    flag: '🇮🇹',
-    imageUrl: 'https://flagcdn.com/it.svg',
-  },
-  {
     label: '日本語',
     value: 'ja',
     flag: '🇯🇵',
     imageUrl: 'https://flagcdn.com/jp.svg',
-  },
-  {
-    label: '中文',
-    value: 'zh',
-    flag: '🇨🇳',
-    imageUrl: 'https://flagcdn.com/cn.svg',
-  },
-  {
-    label: '한국어',
-    value: 'ko',
-    flag: '🇰🇷',
-    imageUrl: 'https://flagcdn.com/kr.svg',
-  },
-  {
-    label: 'العربية',
-    value: 'ar',
-    flag: '🇦🇪',
-    imageUrl: 'https://flagcdn.com/sa.svg',
   },
 ]
 
